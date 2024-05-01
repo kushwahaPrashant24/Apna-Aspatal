a9bd5693b7df2cd27a0a218ee8a81e76fcb98e10
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-05-01 08:52:45 +0530

    added the data

--- src/component/Appointment/Appointment.jsx
+++ src/component/Appointment/Appointment.jsx
@@ -6,7 +6,7 @@ export default function Appointment  () {
   return (
     <div id='Appointment'>
       <h1>BOOK APPOINTMENT</h1>
-      <AppointmentForm />
+      
     </div>
   )
 }
--- src/component/Appointment/AppointmentForm.jsx
+++ src/component/Appointment/AppointmentForm.jsx
@@ -1,26 +1,42 @@
 import React from "react";
+import BasicDatePicker from "./Date.jsx"
+
 
 function AppointmentForm() {
   return (
-    <div>
+    <div id="AppointmentForm">
       <form>
         <label>
-          Name:
+          Full Name:
           <input type="text" name="name" />
         </label>
         <label>
-          Name:
+          Age:
           <input type="text" name="name" />
         </label>
         <label>
-          Name:
+          Gender:
+          <select>
+          <option value="Select Gender">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option selected value="Other">
+              Other
+            </option>
+          </select>
+        </label>
+        <label>
+        Contact Number:
           <input type="text" name="name" />
         </label>
         <label>
-          Name:
+          E-mail:
           <input type="text" name="name" />
         </label>
-        <textarea>Hello there, this is some text in a text area</textarea>
+        /**Appointment Details*/
+      
+        
+        <textarea></textarea>
         <input type="submit" value="Submit" />
       </form>
     </div>
