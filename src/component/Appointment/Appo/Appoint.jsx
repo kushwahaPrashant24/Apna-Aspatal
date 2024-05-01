99254b470d8f055910517d7508a413c2a51b7b81
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-05-01 11:45:33 +0530

    added the form

--- src/component/Appointment/Appo/Appoint.jsx
+++ src/component/Appointment/Appo/Appoint.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 
 function Appoint() {
-    return <div>
-
+  return (
+    <div>
+      <form>
+      
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Age:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Gender:
+          <select>
+            <option value="Select Gender">Select Gender</option>
+            <option value="Male">Male</option>
+            <option selected value="Other">
+              Other
+            </option>
+          </select>
+        </label>
+        
+         <label>
+         Preferred Date :
+         <input type="Date" name="Date" />
+         </label>
+         <label>
+         Preferred Time :
+         <input type="Time" name="Time" />
+         </label>
+         <label>
+         Reason for Visit
+          <textarea/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
+  );
 }
 export default Appoint;
