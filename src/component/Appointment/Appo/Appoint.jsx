08d8561a133f7eff47d1a791efd035e80ad36c8f
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-05-01 11:33:27 +0530

    added the appoint section

---
+++ src/component/Appointment/Appo/Appoint.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Appoint() {
+    return <div>
+
+    </div>
+}
+export default Appoint;
--- src/component/Appointment/Appointment.jsx
+++ src/component/Appointment/Appointment.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import "./Appointment.scss";
-import AppointmentForm from './AppointmentForm';
+import Appoint from './Appo/Appoint';
+
 
 export default function Appointment  () {
   return (
     <div id='Appointment'>
       <h1>BOOK APPOINTMENT</h1>
-      
+      <Appoint />
     </div>
   )
 }
